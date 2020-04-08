d577264dff582dd7dda4e40dcb8c333c2877edba
Jan10e <a.c.broek@gmail.com>
2020-04-08 15:33:14 +0200

    continuous SIR model

--- Main_SIR_continuous.jl
+++ Main_SIR_continuous.jl
@@ -14,7 +14,9 @@ import Random
 Random.seed!(1234)
 randn(42)
 
-sir_out = sir_ct(0.1/1000,0.05,1000,999,1,0,200);
+sir_out = sir_ct(0.1/1000, 0.05, 1000, 999, 1, 0, 200);
+# sir_ct(beta, gamma, N, S0, I0, R0, tf)
+
 head(sir_out)
 
 @df sir_out plot(:time, [:S :I :R], xlabel="Time",ylabel="Number")
